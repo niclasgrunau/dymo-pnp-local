eec5ab2399ffc67c5f8f1c49d0f3b06db3539e1c
Niclas Grunau <niclasgrunau@MacBook-Pro-von-Niclas.local>
2024-04-18 14:48:04 +0200

    „adj“

--- localServer.js
+++ localServer.js
@@ -4,21 +4,32 @@ const path = require("path");
 const app = express();
 const cors = require("cors");
 
+let canvasWidth;
+
 // Enable CORS middleware
 app.use(cors());
 
+// Route to handle the GET request
+app.get("/test-route", (req, res) => {
+  res.status(200).json({ message: "worked" });
+});
+
 // Route to handle image resizing
 app.post("/local-resize", (req, res) => {
+  canvasWidth = req.body.canvasWidth;
+
   // Define the URL of the image to resize
   const imagePath =
     "https://lehre.bpm.in.tum.de/~ge83neb/dymo-pnp/backend/downloads/DYMOPNP_label.png";
 
   // Define the output path for the resized image
   const outputPath = path.join(__dirname, "output.pdf");
+  let canvasWidthPrintingSite = 2 * Math.floor(canvasWidth / 2.91666667);
 
   // Execute the resizing command using ImageMagick
   exec(
-    `convert ${imagePath} -page 531x69 ${outputPath}`,
+    `convert ${imagePath} -page ${canvasWidthPrintingSite}x48 ${outputPath}`,
+
     (error, stdout, stderr) => {
       if (error) {
         // Log and send an error response if there's an error
@@ -36,19 +47,21 @@ app.post("/download-command", (req, res) => {
   try {
     // Define the path of the resized image
     const imagePath = path.join(__dirname, "output.pdf");
+
+    let canvasWidthPrintingSite = Math.floor(canvasWidth / 2.91666667); //this doesnt affect werid scale
+
+    const execCommand = `lp -d DYMO_LabelManager_PnP -o landscape -o PageSize=Custom.24x${canvasWidthPrintingSite} -o fit-to-page ${imagePath}`;
+
     // Execute the print command using CUPS
-    exec(
-      `lp -d DYMO_LabelManager_PnP -o landscape -o PageSize=w35h252 -o fit-to-page ${imagePath}`,
-      (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error: ${error.message}`);
-          return res.status(500).json({ error: "Internal Server Error" });
-        }
-        console.log(`Command output: ${stdout}`);
-        // Send a JSON response indicating successful execution of the print command
-        res.json({ message: "Command executed successfully" });
+    exec(execCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error: ${error.message}`);
+        return res.status(500).json({ error: "Internal Server Error" });
       }
-    );
+      console.log(`Command output: ${stdout}`);
+      // Send a JSON response indicating successful execution of the print command
+      res.status(200).json({ message: "Command executed successfully" });
+    });
   } catch (error) {
     console.error("Error executing command:", error);
     res.status(500).json({ error: "Internal Server Error" });
